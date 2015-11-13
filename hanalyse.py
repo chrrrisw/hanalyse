70da24e9f792eaa707d2db7046b913054be50ecb
Chris Willoughby <chrrrisw@gmail.com>
2015-11-13 13:13:27 +1030

    put tags in a dock, new view menu, move combo box entries to code, tag highlighting start

--- hanalyse.py
+++ hanalyse.py
@@ -20,6 +20,7 @@ It defines:
 import argparse
 import os
 import sys
+import uuid
 from enum import IntEnum
 from PyQt5 import QtWidgets, QtGui, QtCore
 from QHexEdit import QHexEdit, QHexEditData, QHexEditDataReader
@@ -33,16 +34,26 @@ __updated__ = '2014-01-09'
 
 
 class TagTypes(IntEnum):
-    Char = 1
-    Uint8 = 2
-    Uint16 = 3
-    Uint32 = 4
-    Uint64 = 5
-    Int8 = 6
-    Int16 = 7
-    Int32 = 8
-    Int64 = 9
-    String = 10
+    Char = 0
+    Uint8 = 1
+    Uint16 = 2
+    Uint32 = 3
+    Uint64 = 4
+    Int8 = 5
+    Int16 = 6
+    Int32 = 7
+    Int64 = 8
+    String = 9
+    Unknown = 10
+
+
+class TagRoles(IntEnum):
+    Constant = 0
+    Count = 1
+    Offset = 2
+    Signature = 3
+    Size = 4
+    Unknown = 5
 
 
 def uchar_to_uint8(uchar):
@@ -54,6 +65,19 @@ def uchar_to_int8(uchar):
     # 'big' or 'little'
     return (int.from_bytes(uchar, sys.byteorder, signed=True))
 
+
+TYPECOLOURS = {
+    TagTypes.Char: QtGui.QColor.fromHsv(0, 127, 255),
+    TagTypes.Uint8: QtGui.QColor.fromHsv(30, 127, 255),
+    TagTypes.Uint16: QtGui.QColor.fromHsv(60, 127, 255),
+    TagTypes.Uint32: QtGui.QColor.fromHsv(90, 127, 255),
+    TagTypes.Uint64: QtGui.QColor.fromHsv(120, 127, 255),
+    TagTypes.Int8: QtGui.QColor.fromHsv(150, 127, 255),
+    TagTypes.Int16: QtGui.QColor.fromHsv(180, 127, 255),
+    TagTypes.Int32: QtGui.QColor.fromHsv(210, 127, 255),
+    TagTypes.Int64: QtGui.QColor.fromHsv(240, 127, 255),
+    TagTypes.String: QtGui.QColor.fromHsv(270, 127, 255)
+}
 
 TYPEREADERS = {
     TagTypes.Char: ('at',),
@@ -70,11 +94,99 @@ TYPEREADERS = {
 
 
 class Tag(object):
-    def __init__(self):
-        self.parent_tag = None
-        self.identifier = None
-        self.tag_type = None
-        self.tag
+    def __init__(self, parent=None):
+        self._parent_tag = None
+        self._identifier = uuid.uuid4()
+        self._name = None
+        self._start = None
+        self._end = None
+        self._type = None
+        self._role = None
+        self._comment = None
+
+    @property
+    def identifier(self):
+        return self._identifier
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def start(self):
+        return self._start
+
+    @start.setter
+    def start(self, value):
+        if type(value) == str:
+            if value[:2].lower() == '0x':
+                value = int(value, 16)
+            else:
+                value = int(value)
+        self._start = value
+
+    @property
+    def end(self):
+        return self._end
+
+    @end.setter
+    def end(self, value):
+        if type(value) == str:
+            if value[:2].lower() == '0x':
+                value = int(value, 16)
+            else:
+                value = int(value)
+        self._end = value
+
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, value):
+        if type(value) == str:
+            value = TagTypes[value]
+        self._type = value
+
+    @property
+    def role(self):
+        return self._role
+
+    @role.setter
+    def role(self, value):
+        if type(value) == str:
+            value = TagRoles[value]
+        self._role = value
+
+    @property
+    def comment(self):
+        return self._comment
+
+    @comment.setter
+    def comment(self, value):
+        self._comment = value
+
+    def __str__(self):
+        return '''Tag:
+\tParent: {}
+\tName: {}
+\tStart: {}
+\tEnd: {}
+\tType: {}
+\tRole: {}
+\tComment: {}'''.format(
+            self._parent_tag,
+            self._name,
+            self._start,
+            self._end,
+            self._type.name,
+            self._role.name,
+            self._comment)
+
 
 HIGHLIGHT_COLOUR = QtGui.QColor(255, 0, 0)
 
@@ -147,6 +259,17 @@ class SlaveHexEdit(QHexEdit):
         self.setReadOnly(True)
 
 
+def create_action(parent, name, text, shortcut=None):
+    new_action = QtWidgets.QAction(parent)
+    new_action.setObjectName(name)
+    new_action.setText(
+        QtCore.QCoreApplication.translate(parent.objectName(), text))
+    if shortcut is not None:
+        new_action.setShortcut(
+            QtCore.QCoreApplication.translate(parent.objectName(), shortcut))
+    return new_action
+
+
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def __init__(self, parent=None):
@@ -167,16 +290,22 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         layout_2.addWidget(self.hex_2)
 
         # Create context menu in hex 1
-        self._tagAction = QtWidgets.QAction(
-            "Tag", None)
-        self._tagAction.triggered.connect(self.tag_cb)
+        self._tagAction = create_action(
+            self,
+            "contextTag",
+            "Tag")
+        self._tagAction.triggered.connect(self.on_actionTag_triggered)
 
-        self._absOffsetAction = QtWidgets.QAction(
-            "Show absolute offset", None)
+        self._absOffsetAction = create_action(
+            self,
+            'contextAbsOffset',
+            'Show absolute offset')
         self._absOffsetAction.triggered.connect(self.show_abs_offset_cb)
 
-        self._relOffsetAction = QtWidgets.QAction(
-            "Show relative offset", None)
+        self._relOffsetAction = create_action(
+            self,
+            'contextRelOffset',
+            'Show relative offset')
         self._relOffsetAction.triggered.connect(self.show_rel_offset_cb)
 
         self.hex_1.addAction(self._tagAction)
@@ -185,8 +314,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.hex_1.setContextMenuPolicy(QtCore.Qt.ActionsContextMenu)
 
         # Create context menu in hex 2
-        self._findOffset = QtWidgets.QAction(
-            'Find offset', None)
+        self._findOffset = create_action(
+            self,
+            'contextFindOffset',
+            'Find offset')
         self._findOffset.triggered.connect(self.find_offset_cb)
         self.hex_2.addAction(self._findOffset)
         self.hex_2.setContextMenuPolicy(QtCore.Qt.ActionsContextMenu)
@@ -194,27 +325,36 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # Connect signals to slots
         self.hex_1.show_offset.connect(self.hex_2.setCursorPos)
 
-        # Connect up the menu
-        self.actionOpen.triggered.connect(self.open_cb)
-        self.actionTag.triggered.connect(self.tag_cb)
-        self.actionQuit.triggered.connect(self.quit_cb)
-
         # Dialogs
         self._tag_dialog = QtWidgets.QDialog(self)
         self._tag_contents = Ui_Tag()
         self._tag_contents.setupUi(self._tag_dialog)
+        for tag_type in TagTypes:
+            self._tag_contents.type_combobox.addItem('')
+            self._tag_contents.type_combobox.setItemText(
+                tag_type.value,
+                QtCore.QCoreApplication.translate(
+                    self.objectName(), tag_type.name))
+        for tag_role in TagRoles:
+            self._tag_contents.role_combo.addItem('')
+            self._tag_contents.role_combo.setItemText(
+                tag_role.value,
+                QtCore.QCoreApplication.translate(
+                    self.objectName(), tag_role.name))
 
         # Internal stuff
         self._hexeditdata = None
         self._hexeditdatareader = None
+        self._tags = []
 
-    # def open_file(self, filename):
-    #     self.hex_1.show_file(filename[0])
-    #     self.hex_2.show_file(filename[0])
-
-    def open_cb(self):
+    @QtCore.pyqtSlot()
+    def on_actionOpen_triggered(self):
+        print('on_actionOpen_triggered')
         filename = QtWidgets.QFileDialog.getOpenFileName(
-            self, 'Load File', '.', '*')
+            parent=self,
+            caption='Load File',
+            directory='.',
+            filter='*')
         if filename[0] != '':
             self._hexeditdata = QHexEditData.fromFile(filename[0])
             self._hexeditdatareader = QHexEditDataReader(
@@ -222,45 +362,79 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 self)
             self.hex_1.setData(self._hexeditdata)
             self.hex_2.setData(self._hexeditdata)
-            # self.open_file(filename[0])
 
-    def tag_cb(self):
-        # TODO: Should we get the temp highlight instead of selection?
-        self._tag_contents.extents_start_lineedit.setText(
-            '0x{:08x}'.format(self.hex_1.selectionStart()))
-        self._tag_contents.extents_end_lineedit.setText(
-            '0x{:08x}'.format(self.hex_1.selectionEnd()))
-        result = self._tag_dialog.exec_()
+    @QtCore.pyqtSlot()
+    def on_actionTag_triggered(self):
+        if self._hexeditdatareader is not None:
+            # TODO: Should we get the temp highlight instead of selection?
+            self._tag_contents.extents_start_lineedit.setText(
+                '0x{:08x}'.format(self.hex_1.selectionStart()))
+            self._tag_contents.extents_end_lineedit.setText(
+                '0x{:08x}'.format(self.hex_1.selectionEnd()))
+            result = self._tag_dialog.exec_()
 
-        if result:
-            pass
+            if result:
+                new_tag = Tag()
+                new_tag.name = self._tag_contents.tag_lineedit.text()
+                new_tag.start = self._tag_contents.extents_start_lineedit.text()
+                new_tag.end = self._tag_contents.extents_end_lineedit.text()
+                new_tag.type = self._tag_contents.type_combobox.currentText()
+                new_tag.role = self._tag_contents.role_combo.currentText()
+                new_tag.comment = self._tag_contents.comment_textedit.toPlainText()
+
+                # Store it
+                self._tags.append(new_tag)
+
+                # TODO: Colour it
+                self.hex_1.highlightBackground(
+                    new_tag.start,
+                    new_tag.end,
+                    TYPECOLOURS[new_tag.type])
+
+    @QtCore.pyqtSlot()
+    def on_actionQuit_triggered(self):
+        print('on_actionQuit_triggered')
+        self.close()
+
+    @QtCore.pyqtSlot(bool)
+    def on_actionShowTags_triggered(self, checked):
+        '''Called on user triggering the action, not on programmatic change.'''
+        print('on_actionShowTags_triggered')
+
+    # @QtCore.pyqtSlot(bool)
+    # def on_actionShowTags_toggled(self, checked):
+    #     print('on_actionShowTags_toggled')
+
+    @QtCore.pyqtSlot(bool)
+    def on_actionShowSlave_triggered(self, checked):
+        '''Called on user triggering the action, not on programmatic change.'''
+        print('on_actionShowSlave_triggered')
 
     def show_abs_offset_cb(self):
-        sel_start = self.hex_1.selectionStart()
-        sel_length = self.hex_1.selectionLength()
-        data = self._hexeditdatareader.read(sel_start, sel_length + 1)
-        print(data)
-        offset = int.from_bytes(data, sys.byteorder, signed=False)
-        self.hex_2.setCursorPos(offset)
+        if self._hexeditdatareader is not None:
+            sel_start = self.hex_1.selectionStart()
+            sel_length = self.hex_1.selectionLength()
+            data = self._hexeditdatareader.read(sel_start, sel_length + 1)
+            # print(data)
+            offset = int.from_bytes(data, sys.byteorder, signed=False)
+            self.hex_2.setCursorPos(offset)
 
     def show_rel_offset_cb(self):
         pass
-
-    def quit_cb(self):
-        self.close()
 
     def allow_close(self):
         return True
 
     def find_offset_cb(self):
-        length = 4
-        current_pos = self.hex_2.cursorPos().to_bytes(
-            length,
-            sys.byteorder,
-            signed=False)
-        found_pos = self._hexeditdatareader.indexOf(current_pos, 0)
-        if found_pos > 0:
-            self.hex_1.show_search_result(found_pos, length)
+        if self._hexeditdatareader is not None:
+            length = 4
+            current_pos = self.hex_2.cursorPos().to_bytes(
+                length,
+                sys.byteorder,
+                signed=False)
+            found_pos = self._hexeditdatareader.indexOf(current_pos, 0)
+            if found_pos > 0:
+                self.hex_1.show_search_result(found_pos, length)
 
     def closeEvent(self, event):
         if self.allow_close:
